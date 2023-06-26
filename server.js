b7f10fc51e1af4b06627777f1edddcd7f75cabf3
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-06-26 11:39:32 +0800

    Updated server with successful call on Postman

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const { response } = require('express');
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
 
 const app = express();
 
+const accountKey = "FbQwubspQs+LjagFP5qsUA==";
+
 app.use((req, res, next) => {
-    req.header('AccountKey', 'FbQwubspQs+LjagFP5qsUA==');
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-app.get('/servicealerts', (req, res) => {
-    request({
-        url: 'http://datamall2.mytransport.sg/ltaodataservice/TrainServiceAlerts',
-    },
-        (error, response, body) => {
-            if (error || response.statusCode !== 200) {
-                console.log(response.statusCode)
-                console.log(req.headers)
-                return res.status(response.statusCode).json({ type : 'error', message : req.body });
-            }
+const requestOptions = {
+  url: "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139",
+  method: "GET",
+  json: {},
+  headers: {
+    AccountKey: accountKey,
+  },
+};
 
-            res.json(JSON.parse(body));
-        }
-    )
+app.get("/servicealerts", (req, res) => {
+  request(requestOptions, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      res.status(response.statusCode).json({ type: "error", message: "" });
+    } else {
+      res.send(body);
+    }
+
+    // res.json(JSON.parse(body));
+  });
 });
 
 const PORT = process.env.PORT || 3000;
