07aab19a3708edadba91035acbae945e66df9630
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-06-26 13:53:57 +0800

    Added PlatformCrowdForecast API and replaced generic error messages

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const desiredURLs = {
     "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2",
   platformCrowdURL:
     "http://datamall2.mytransport.sg/ltaodataservice/PCDRealTime?TrainLine=",
+  crowdForecastURL: 
+    "http://datamall2.mytransport.sg/ltaodataservice/PCDForecast?TrainLine="
 };
 
 app.use((req, res, next) => {
@@ -45,17 +47,22 @@ const platformCrowd = {
   headers,
 };
 
+const crowdForecast = {
+  url: desiredURLs.crowdForecastURL,
+  method: "GET",
+  json: {},
+  headers
+}
+
 app.get("/servicealerts", (req, res) => {
   request(serviceAlert, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       res
         .status(response.statusCode)
-        .json({ type: "error", message: error.message });
+        .json({ type: "error", message: "Refer to status returned by response to continue troubleshooting."});
     } else {
       res.send(body);
     }
-
-    // res.json(JSON.parse(body));
   });
 });
 
@@ -64,7 +71,7 @@ app.get("/carparkavailability", (req, res) => {
     if (error || response.statusCode !== 200) {
       res
         .status(response.statusCode)
-        .json({ type: "error", message: error.message });
+        .json({ type: "error", message: "Refer to status returned by response to continue troubleshooting." });
     } else {
       res.send(body);
     }
@@ -73,8 +80,6 @@ app.get("/carparkavailability", (req, res) => {
 
 app.get("/platformcrowd", (req, res) => {
   request(platformCrowd, (error, response, body) => {
-    // console.log(req.query)
-    // res.send(req.query)
     if (req.query.TrainLine) {
       platformCrowd.url = desiredURLs.platformCrowdURL + req.query.TrainLine;
     } else {
@@ -87,12 +92,33 @@ app.get("/platformcrowd", (req, res) => {
     }
 
     if (error || response.statusCode !== 200) {
-      res.status(response.statusCode).json({ type: "error", message: "" });
+      res.status(response.statusCode).json({ type: "error", message: "Refer to status returned by response to continue troubleshooting." });
     } else {
       res.send(body);
     }
   });
 });
+
+app.get("/crowdforecast", (req, res) => {
+  request(crowdForecast, (error, response, body) => {
+    if (req.query.TrainLine) {
+      crowdForecast.url = desiredURLs.crowdForecastURL + req.query.TrainLine;
+    } else {
+      res
+        .status(400)
+        .json({
+          type: "error",
+          message: "Missing query string for train line.",
+        });
+    }
+
+    if (error || response.statusCode !== 200) {
+      res.status(response.statusCode).json({ type: "error", message: "Refer to status returned by response to continue troubleshooting." });
+    } else {
+      res.send(body);
+    }
+  });
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
