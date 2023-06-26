119f60ff2a65e16550a3207318b05075151fab0c
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-06-26 13:47:24 +0800

    Added APIs for other functions of application (Platform Crowd, Carpark Availability) and their respective routes

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const app = express();
 
 const accountKey = "FbQwubspQs+LjagFP5qsUA==";
 
-const desiredURL = "http://datamall2.mytransport.sg/ltaodataservice/TrainServiceAlerts";
+const headers = {
+  AccountKey: accountKey,
+};
+
+const desiredURLs = {
+  serviceAlertURL:
+    "http://datamall2.mytransport.sg/ltaodataservice/TrainServiceAlerts",
+  carparkAvailabilityURL:
+    "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2",
+  platformCrowdURL:
+    "http://datamall2.mytransport.sg/ltaodataservice/PCDRealTime?TrainLine=",
+};
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,24 +24,73 @@ app.use((req, res, next) => {
   next();
 });
 
-const requestOptions = {
-  url: desiredURL,
+const serviceAlert = {
+  url: desiredURLs.serviceAlertURL,
   method: "GET",
   json: {},
-  headers: {
-    AccountKey: accountKey,
-  },
+  headers,
+};
+
+const carparkAvailability = {
+  url: desiredURLs.carparkAvailabilityURL,
+  method: "GET",
+  json: {},
+  headers,
+};
+
+const platformCrowd = {
+  url: desiredURLs.platformCrowdURL,
+  method: "GET",
+  json: {},
+  headers,
 };
 
 app.get("/servicealerts", (req, res) => {
-  request(requestOptions, (error, response, body) => {
+  request(serviceAlert, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      res.status(response.statusCode).json({ type: "error", message: "" });
+      res
+        .status(response.statusCode)
+        .json({ type: "error", message: error.message });
     } else {
       res.send(body);
     }
 
     // res.json(JSON.parse(body));
+  });
+});
+
+app.get("/carparkavailability", (req, res) => {
+  request(carparkAvailability, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      res
+        .status(response.statusCode)
+        .json({ type: "error", message: error.message });
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.get("/platformcrowd", (req, res) => {
+  request(platformCrowd, (error, response, body) => {
+    // console.log(req.query)
+    // res.send(req.query)
+    if (req.query.TrainLine) {
+      platformCrowd.url = desiredURLs.platformCrowdURL + req.query.TrainLine;
+    } else {
+      res
+        .status(400)
+        .json({
+          type: "error",
+          message: "Missing query string for train line.",
+        });
+    }
+
+    if (error || response.statusCode !== 200) {
+      res.status(response.statusCode).json({ type: "error", message: "" });
+    } else {
+      res.send(body);
+    }
   });
 });
 
